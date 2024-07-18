d3cca28c0a7efcf8658096ad23d5fcbfcd87820e
sujan <budhasujan3@gmail.com>
2024-07-18 23:42:33 +0545

    Importing and exporting multiple components from same file using named and default

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+// default export statement:: 
+// Exporting:->export default function Gallery(){}
+// Importing:-> import Gallery from './Gallery.jsx';
+
+//export function Profile() {}
+//import {Profile} from './Profile.jsx';
+
+import Gallery from "./Gallery"
+import { Profile } from "./Profile"
 
 function App() {
 
   return (
     <>
-  <h1>Hello World...</h1>
+      <h1>Hello World...</h1>
+      <Profile/>
+      <Gallery/>
     </>
   )
 }
